457efa015c9de07f674ee68d5a663e22c110cd4b
Iago Nunes <iago-lucas@hotmail.com>
2020-11-13 15:36:31 -0300

    add

---
+++ backend/models/Agendamento.js
@@ -0,0 +1,33 @@
+
+module.exports = (sequelize, DataTypes) => {
+
+    const agendamento = sequelize.define(
+        'Agendamento',
+        {
+            horarioAgendadoInicio: DataTypes.STRING,
+            horarioAgendadoFim: DataTypes.STRING,
+            googleId: DataTypes.STRING,
+            clienteNome: DataTypes.STRING,
+            clienteEmail: DataTypes.STRING,
+            clienteTelefone: DataTypes.STRING,
+            eventos_id:{
+                type:DataTypes.INTEGER,
+                foreignKey:true
+            }
+            
+        },{
+            tableName:"agendamento",
+            timestamps:true
+        }
+    )
+    agendamento.associate = (models) =>{
+        agendamento.belongsTo(models.Evento,{as:"agendamentos_evento",foreignKey:"eventos_id"}) 
+        agendamento.belongsToMany(models.Pergunta, {as:'agendamento_pergunta',through:"respostas",foreignKey:"pergunta_id",otherKey:"perguntas_id"})
+
+    // usuario.hasMany(models.Post,{as:"posts",foreignKey:"usuarios_id"})
+    // usuario.belongsToMany(models.Post, {as:'usuariocurtiu',through:"curtidas",foreignKey:"usuarios_id",otherKey:"posts_id"})
+  }
+  
+    return agendamento;
+    
+  }
---
+++ backend/models/Pergunta.js
@@ -0,0 +1,26 @@
+
+module.exports = (sequelize, DataTypes) => {
+
+    const pergunta = sequelize.define(
+        'Pergunta',
+        {
+            perguntas: DataTypes.STRING,
+            eventos_id:{
+                type:DataTypes.INTEGER,
+                foreignKey:true
+            }
+            
+        },{
+            tableName:"perguntas",
+            timestamps:true
+        }
+    )
+    pergunta.associate = (models) =>{
+        pergunta.belongsTo(models.Evento,{as:"pergunta_evento",foreignKey:"eventos_id"}) 
+    // usuario.hasMany(models.Post,{as:"posts",foreignKey:"usuarios_id"})
+    pergunta.belongsToMany(models.Agendamento, {as:'pergunta_agendamento',through:"respostas",foreignKey:"agendamento_id",otherKey:"perguntas_id"})
+  }
+  
+    return pergunta;
+    
+  }
